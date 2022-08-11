b82e14c23c4e324cd614fe8abc403ef9cb5ee8f6
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-08-11 08:03:46 -0400

    created Person and PersonController classes

---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,26 @@
+package io.zipcoder.crudapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonController {
+    public Person createPerson(Person p){
+        return null;
+    }
+
+    public Person getPerson(int id){
+        return null;
+    }
+
+    List<Person> getPersonList(){
+        return null;
+    }
+
+    public Person updatePerson(Person p){
+        return null;
+    }
+
+    public void DeletePerson(int id){
+
+    }
+}
